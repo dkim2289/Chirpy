5eed70e97611106c74849e8cde2c75501717d347
dkim2289 <dhkim.89.1497@icloud.com>
2026-06-23 22:31:14 +1000

    and adding a query accordingly

--- internal/database/users.sql.go
+++ internal/database/users.sql.go
@@ -26,7 +26,7 @@ values (
     $1,
     $2
 )
-returning id, created_at, updated_at, email, hashed_password
+returning id, created_at, updated_at, email, hashed_password, is_chirpy_red
 `
 
 type CreateUserParams struct {
@@ -43,6 +43,7 @@ func (q *Queries) CreateUser(ctx context.Context, arg CreateUserParams) (User, e
 		&i.UpdatedAt,
 		&i.Email,
 		&i.HashedPassword,
+		&i.IsChirpyRed,
 	)
 	return i, err
 }
@@ -50,7 +51,7 @@ func (q *Queries) CreateUser(ctx context.Context, arg CreateUserParams) (User, e
 const updateUser = `-- name: UpdateUser :one
 UPDATE users SET email = $1, hashed_password = $2, updated_at = NOW()
 WHERE id = $3
-RETURNING id, created_at, updated_at, email, hashed_password
+RETURNING id, created_at, updated_at, email, hashed_password, is_chirpy_red
 `
 
 type UpdateUserParams struct {
@@ -68,6 +69,32 @@ func (q *Queries) UpdateUser(ctx context.Context, arg UpdateUserParams) (User, e
 		&i.UpdatedAt,
 		&i.Email,
 		&i.HashedPassword,
+		&i.IsChirpyRed,
+	)
+	return i, err
+}
+
+const updateUserIsChirpyRed = `-- name: UpdateUserIsChirpyRed :one
+UPDATE users SET is_chirpy_red = $1, updated_at = NOW()
+WHERE id = $2
+RETURNING id, created_at, updated_at, email, hashed_password, is_chirpy_red
+`
+
+type UpdateUserIsChirpyRedParams struct {
+	IsChirpyRed bool
+	ID          uuid.UUID
+}
+
+func (q *Queries) UpdateUserIsChirpyRed(ctx context.Context, arg UpdateUserIsChirpyRedParams) (User, error) {
+	row := q.db.QueryRowContext(ctx, updateUserIsChirpyRed, arg.IsChirpyRed, arg.ID)
+	var i User
+	err := row.Scan(
+		&i.ID,
+		&i.CreatedAt,
+		&i.UpdatedAt,
+		&i.Email,
+		&i.HashedPassword,
+		&i.IsChirpyRed,
 	)
 	return i, err
 }
--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -20,3 +20,9 @@ returning *;
 UPDATE users SET email = $1, hashed_password = $2, updated_at = NOW()
 WHERE id = $3
 RETURNING *;
+
+
+-- name: UpdateUserIsChirpyRed :one
+UPDATE users SET is_chirpy_red = $1, updated_at = NOW()
+WHERE id = $2
+RETURNING *;
